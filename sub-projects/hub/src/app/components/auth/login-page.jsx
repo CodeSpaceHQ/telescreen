a71c507d8b68f84e75cb24fad4794c9f06b7b4b4
Rachel <rachelcramblitt@gmail.com>
2017-10-19 02:06:13 -0500

    WIP: inputs there need to work on CSS and authentication

--- sub-projects/hub/src/app/components/auth/login-page.jsx
+++ sub-projects/hub/src/app/components/auth/login-page.jsx
@@ -1,15 +1,58 @@
 import React from 'react';
-import {Button} from 'semantic-ui-react';
+import { Button, Form, Input, TextArea, Image, Icon} from 'semantic-ui-react';
+
+import binaryEye from './Binary-Eye-2.png';
 
 export default class LoginPage extends React.Component{ 
-  
-  
+    
+
+    constructor(){
+        super();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInputChange(){
+        console.log("Stuff changed");
+    }
+
+    handleSubmit() {
+        console.log("Clicked Login!!");
+     }
   render(){
         return(
-            <div>
-                <Button>Login Page</Button>
-            </div> 
-        
+            <div className="ui middle aligned center aligned grid">
+                {/* <Image src={binaryEye}/> */}
+
+                <Form onSubmit={this.handleSubmit}>
+                <fieldset>
+                    <Form.Field className="ui left icon input">
+                        <Icon name='user'/>                    
+                        <input
+                            name='email'
+                            type='text'
+                            placeholder='Email Address'
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Field>
+                    <Form.Field className="ui left icon input">
+                        <Icon name='lock'/>  
+                        <input
+                            name='password'
+                            type='text'
+                            placeholder='Password'
+                            onChange={this.handleInputChange}
+                        />
+                    </Form.Field>
+                </fieldset>
+
+                    <Button 
+                        type='submit'
+                        primary
+                        size='large'>Login
+                    </Button>
+                </Form>          
+            </div>   
     );
   }
 }
