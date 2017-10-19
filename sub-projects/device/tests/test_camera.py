028d8beb218c88b2fda5e07faa3db3cc28098b3c
dlowrey <dane.r.lowrey@gmail.com>
2017-10-19 14:18:00 -0500

    Removed detection stuff

--- sub-projects/device/tests/test_camera.py
+++ sub-projects/device/tests/test_camera.py
@@ -9,7 +9,7 @@ video_stream.start()
 while True:
     frame = video_stream.read() # read a frame from the stream
     cv2.imshow('img', frame)  # display the frame in a window
-    if cv2.waitKey(1) == ord('q'):  # must follow imshow
+    if cv2.waitKey(1) == ord('q'):  # must follow imshow, wait for keypress
         break  # stop if 'q' is pressed
 
 video_stream.stop()
