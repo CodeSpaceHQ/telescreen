259f3c57d14fd58a0716bdad574dfb3397d41dbf
Rachel <rachelcramblitt@gmail.com>
2017-10-24 17:53:59 -0500

    improvements to css for login page (still WIP)

--- sub-projects/hub/src/app/components/auth/login-page.jsx
+++ sub-projects/hub/src/app/components/auth/login-page.jsx
@@ -1,58 +1,105 @@
 import React from 'react';
-import { Button, Form, Input, TextArea, Image, Icon} from 'semantic-ui-react';
+import { Button, Form, Input, Header, TextArea, Image, Icon, Container, Grid, Segment, GridColumn } from 'semantic-ui-react';
 
 import binaryEye from './Binary-Eye-2.png';
 
-export default class LoginPage extends React.Component{ 
-    
+export default class LoginPage extends React.Component {
 
-    constructor(){
-        super();
-        this.handleInputChange = this.handleInputChange.bind(this);
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            submittedName: '',
+            submittedEmail: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    handleInputChange(){
-        console.log("Stuff changed");
+    handleChange(e, stuff) {
+        this.setState({ [name]: value })
+        //Working on this still!!!!!!!!!!!!!
     }
 
     handleSubmit() {
-        console.log("Clicked Login!!");
-     }
-  render(){
-        return(
-            <div className="ui middle aligned center aligned grid">
+        console.log("Clicked Login");
+        // const { name, email } = this.state
+        // this.setState({ submittedName: name, submittedEmail: email })
+        // console.log("submitted name is: "+ submittedName);
+        // request.post(
+        //     'http://127.0.0.1:3000/api/auth',
+        //     { json: { name: JSON.stringify(name) } },
+        //     function (error, response, body) {
+        //         if (!error && response.statusCode == 200) {
+        //             console.log("request successful")
+        //         }
+        //         else {
+        //             console.log("error: " + error);
+        //         }
+        //     }
+        // );
+        //Working on this still!!!!!!!!!!!
+    }
+    render() {
+        return (
+            <div className='login-form'>
+                <style>{`
+                    body > div,
+                    body > div > div,
+                    body > div > div > div.login-form {
+                      height: 100%;
+                    }
+                `}</style>
+
                 {/* <Image src={binaryEye}/> */}
 
-                <Form onSubmit={this.handleSubmit}>
-                <fieldset>
-                    <Form.Field className="ui left icon input">
-                        <Icon name='user'/>                    
-                        <input
-                            name='email'
-                            type='text'
-                            placeholder='Email Address'
-                            onChange={this.handleInputChange}
-                        />
-                    </Form.Field>
-                    <Form.Field className="ui left icon input">
-                        <Icon name='lock'/>  
-                        <input
-                            name='password'
-                            type='text'
-                            placeholder='Password'
-                            onChange={this.handleInputChange}
-                        />
-                    </Form.Field>
-                </fieldset>
+                <Grid
+                    textAlign='center'
+                    style={{ height: '100%' }}
+                    verticalAlign='middle'
+                >
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header
+                            as='h2'
+                            color='blue'
+                            textAlign='center'
+                        >
+                            Welcome to Telescreen!
+                        </Header>
+                            <Segment>
+                                <Form className="ui large form" onSubmit={this.handleSubmit}>
+                                    <div>
+                                        <Form.Field className="ui left icon input">
+                                            <Icon name='user' />
+                                            <Input
+                                                name='email'
+                                                type='text'
+                                                placeholder='Email Address'
+                                                onChange={this.handleChange}
+                                            />
+                                        </Form.Field>
+                                        <Form.Field className="ui left icon input">
+                                            <Icon name='lock' />
+                                            <Input
+                                                name='password'
+                                                type='text'
+                                                placeholder='Password'
+                                                onChange={this.handleChange}
+                                            />
+                                        </Form.Field>
 
-                    <Button 
-                        type='submit'
-                        primary
-                        size='large'>Login
-                    </Button>
-                </Form>          
-            </div>   
-    );
-  }
+                                        <Button
+                                            type='submit'
+                                            primary
+                                            size='large'>Login
+                                        </Button>
+                                    </div>
+                                </Form>
+                        </Segment>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        );
+    }
 }
