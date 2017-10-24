e6f43e476d21cf66d4cd4404a92853d4d776498b
jowoodson <joyellealina@gmail.com>
2017-10-23 19:07:32 -0500

    added some styling, still need vertical aligning

--- sub-projects/hub/src/app/components/admin/add-admin.jsx
+++ sub-projects/hub/src/app/components/admin/add-admin.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Grid, Segment } from 'semantic-ui-react';
 
 export default class AddAdmin extends React.Component {
   constructor(props) {
@@ -10,7 +10,7 @@ export default class AddAdmin extends React.Component {
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -22,24 +22,41 @@ export default class AddAdmin extends React.Component {
       [name]: value,
     });
   }
+
+  handleSubmit() {
+    console.log('Button was clicked');
+  }
+
   render() {
     return (
-      <Form>
-        <Form.Input
-          fluid
-          required
-          label='E-mail Address'
-          icon='user'
-          iconPosition='left'
-          placeholder='E-mail Address'
-          onChange={this.handleChange}
-        />
-        <Button
-          primary
-          icon='checkmark'
-          content='Submit'
-        />
-      </Form>
+      <Grid
+        textAlign='center'
+        style={{ height: '100%' }}
+        verticalAlign='middle'
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Segment>
+            <Form>
+              <Form.Input
+                fluid
+                required
+                label='E-mail Address'
+                icon='user'
+                iconPosition='left'
+                placeholder='E-mail Address'
+                onChange={this.handleChange}
+              />
+              <Button
+                primary
+                fluid
+                icon='checkmark'
+                content='Submit'
+                onClick={this.handleSubmit}
+              />
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
