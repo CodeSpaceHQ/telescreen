336ffb7732115371015317a8894ded630e0ee2e7
NilsG-S <nils.griswold.steiner@gmail.com>
2017-10-31 18:48:06 -0500

    Added getRefreshToken

--- sub-projects/hub/src/server/oauth/oauth-manager.js
+++ sub-projects/hub/src/server/oauth/oauth-manager.js
@@ -1,46 +1,71 @@
-const oauth = require('./oauth-models.js');
+const {
+  Token,
+  Refresh,
+  Code,
+} = require('./oauth-models.js');
+const logger = require('utils/logger.js');
 
-function generateAccessToken() {
+async function generateAccessToken() {
+  logger.info('Generating access token.');
+
+  return Token.genToken();
+}
+
+async function generateRefreshToken() {
+  logger.info('Generating refresh token.');
+
+  return Refresh.genRefresh();
+}
+
+async function generateAuthorizationCode() {
+  logger.info('Generating authorization code.');
+
+  return Code.genCode();
+}
+
+async function getRefreshToken(token) {
+  logger.info('Getting refresh token.');
+
+  try {
+    const refresh = await Refresh
+      .findOne({ refresh: token.refresh })
+      .populate('Client')
+      .exec();
+
+    return {
+      refreshToken: token,
+      client: refresh.Client,
+    };
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getAuthorizationCode() {
 
 }
 
-function generateRefreshToken() {
+async function getClient() {
 
 }
 
-function generateAuthorizationCode() {
+async function saveToken() {
 
 }
 
-function getRefreshToken() {
+async function saveAuthorizationCode() {
 
 }
 
-function getAuthorizationCode() {
+async function revokeToken() {
 
 }
 
-function getClient() {
+async function revokeAuthorizationCode() {
 
 }
 
-function saveToken() {
-
-}
-
-function saveAuthorizationCode() {
-
-}
-
-function revokeToken() {
-
-}
-
-function revokeAuthorizationCode() {
-
-}
-
-function validateScope() {
+async function validateScope() {
 
 }
 
