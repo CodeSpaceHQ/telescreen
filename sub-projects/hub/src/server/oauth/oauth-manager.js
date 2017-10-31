737c1bd6c56a57af91c6121591b546a0d3dfaa81
NilsG-S <nils.griswold.steiner@gmail.com>
2017-10-31 14:53:25 -0500

    Setup for node-oauth2-server

---
+++ sub-projects/hub/src/server/oauth/oauth-manager.js
@@ -0,0 +1,59 @@
+const oauth = require('./oauth-models.js');
+
+function generateAccessToken() {
+
+}
+
+function generateRefreshToken() {
+
+}
+
+function generateAuthorizationCode() {
+
+}
+
+function getRefreshToken() {
+
+}
+
+function getAuthorizationCode() {
+
+}
+
+function getClient() {
+
+}
+
+function saveToken() {
+
+}
+
+function saveAuthorizationCode() {
+
+}
+
+function revokeToken() {
+
+}
+
+function revokeAuthorizationCode() {
+
+}
+
+function validateScope() {
+
+}
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  generateAuthorizationCode,
+  getRefreshToken,
+  getAuthorizationCode,
+  getClient,
+  saveToken,
+  saveAuthorizationCode,
+  revokeToken,
+  revokeAuthorizationCode,
+  validateScope,
+};
