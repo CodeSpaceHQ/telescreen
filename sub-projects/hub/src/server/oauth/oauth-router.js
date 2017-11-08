dbf811164193c28d19c226adfc312224294e9e30
NilsG-S <nils.griswold.steiner@gmail.com>
2017-11-07 19:40:25 -0600

    Added basic oauth endpoints

---
+++ sub-projects/hub/src/server/oauth/oauth-router.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const OAuthServer = require('oauth2-server');
+
+const oauthManager = require('./oauth-manager.js');
+
+const oauth = new OAuthServer({
+  model: oauthManager,
+});
+
+const Request = OAuthServer.Request;
+const Response = OAuthServer.Response;
+const router = express.Router();
+
+router.post('/token', (request, response) => {
+  const req = new Request(request);
+  const res = new Response(response);
+
+  oauth.token(req, res)
+    .then((stuff) => {
+      response.status(200).json({ stuff }).end();
+    })
+    .catch((stuff) => {
+      response.status(400).json({ stuff }).end();
+    });
+});
+
+router.post('/authorize', (request, response) => {
+  const req = new Request(request);
+  const res = new Response(response);
+
+  oauth.authorize(req, res)
+    .then((stuff) => {
+      response.status(200).json({ stuff }).end();
+    })
+    .catch((stuff) => {
+      response.status(400).json({ stuff }).end();
+    });
+});
+
+module.exports = { router };
