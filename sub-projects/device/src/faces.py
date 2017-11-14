16cd6ec0382e24d60df1b18d306cfdd6a39d0e38
dlowrey <dane.r.lowrey@gmail.com>
2017-11-14 12:31:24 -0600

    Added outline of FaceTracker class

---
+++ sub-projects/device/src/faces.py
@@ -0,0 +1,9 @@
+
+
+class FaceTracker(object):
+    """
+    A class that takes an input stream of image frames and attempts to detect
+    and track any faces within those frames.
+    """
+    def __init__(self):
+        pass
