984194a491d7ae21e0243880246bad591134fb6f
Rachel <rachelcramblitt@gmail.com>
2017-11-15 17:41:55 -0600

    implemented auth endpoint function

--- sub-projects/hub/src/app/config.jsx
+++ sub-projects/hub/src/app/config.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { HashRouter, Route, Redirect } from 'react-router-dom';
-import axios from 'axios';
 
 import LoginPage from './components/auth/login-page.jsx';
 import AddAdmin from './components/admin/add-admin.jsx';
-
-axios.defaults.withCredentials = true;
 
 const AppConfig = () => (
   <HashRouter>
