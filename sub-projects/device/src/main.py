476055e9277b90961b7ad4e91b6ec259ad9d55a5
dlowrey <dane.r.lowrey@gmail.com>
2017-11-16 15:34:43 -0600

    updated comments in `main.py`

--- sub-projects/device/src/main.py
+++ sub-projects/device/src/main.py
@@ -15,27 +15,34 @@ classifier = cv2.CascadeClassifier(
 # create a tracker that uses the classifier
 tracker = FacesTracker(classifier)
 
-
+# count frames
 frame_count = 0
+# count detected faces
 faces_count = 0
 
+# while the 'q' key has not been pressed
 while cv2.waitKey(1) != ord('q'):
     frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # detect new faces every 15th frame
     if frame_count == 15:
-        # detect new faces every 15th frame
         new_count = tracker.detect(gray_frame)
         if new_count > faces_count:
             # new face found, recognize
+            # replace with call to recognition method
             pass
         faces_count = new_count
         frame_count = 0
     else:
         # update object trackers
         tracker.update(gray_frame)
-    
+
+    # get the coordinates of the tracked faces in the frame
     coordinates = tracker.get_coordinates()
-    
+
+    # draw a rectangle around each face found.
+    # this is only for testing and verification
     for (x, y, w, h) in coordinates:
         cv2.rectangle(frame,  # image to draw on
                       (x,y),  # start position
@@ -44,6 +51,7 @@ while cv2.waitKey(1) != ord('q'):
                       2)  # thickness
     frame_count += 1
     # display frame
+    # this is only for testing and verification
     cv2.imshow('Image', frame)
 
     
