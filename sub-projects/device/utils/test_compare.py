9f64b5446b73b9b39dfda283f65ff278e390d633
camcarlton <cameron.carlton@ttu.edu>
2017-11-20 17:31:15 -0600

    Added Comments
    Added comments to the test_compare.py to know what is going on within the file.

--- sub-projects/device/utils/test_compare.py
+++ sub-projects/device/utils/test_compare.py
@@ -3,17 +3,16 @@ import base64
 
 rek = Rekgoniton()
 
-#opens the image and stores the binary code in b
-with open("caleb.jpg", "rb") as imageFile:
+#this should return 'No match found'
+with open("caleb.jpg", "rb") as imageFile: #opens the image and stores the binary code in b
   f = imageFile.read()
   b = bytearray(f)
 
 rek.compare(b) #calls the compare function
 
-'''
+#this should run successfully and return nothing as of now
 with open("cameron.jpg", "rb") as imageFile:
   f = imageFile.read()
   b = bytearray(f)
 
 Rekgoniton.compare(b)
-'''
