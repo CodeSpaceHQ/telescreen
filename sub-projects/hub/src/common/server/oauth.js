fe657be481f739b19f0441e7fbf2d4eabfe72fc5
NilsG-S <nils.griswold.steiner@gmail.com>
2017-11-26 17:17:11 -0600

    Added basic code exchange functionality

---
+++ sub-projects/hub/src/common/server/oauth.js
@@ -0,0 +1,68 @@
+import Connection from './connection.js';
+import OAuthManager from './oauth-manager.js';
+
+export async function authorize(state) {
+  await OAuthManager.confirmAccess();
+
+  const res = await new Connection()
+    .post()
+    .oauth()
+    .authorize()
+    .data({
+      access_token: OAuthManager.getAccess(),
+      state,
+      response_type: 'code',
+      client_id: OAuthManager.getClientID(),
+    })
+    .call();
+
+  return {
+    code: res.authorizationCode,
+    codeExpires: res.expiresAt,
+  };
+}
+
+export async function token(code) {
+  const res = await new Connection()
+    .post()
+    .oauth()
+    .token()
+    .data({
+      code,
+      grant_type: 'authorization_code',
+      client_id: OAuthManager.getClientID(),
+      redirect_uri: OAuthManager.getClientRedirect(),
+    })
+    .call();
+
+  OAuthManager.setAccess(res.accessToken);
+  OAuthManager.setAccessExpires(res.accessTokenExpiresAt);
+  OAuthManager.setRefresh(res.refreshToken);
+
+  return {
+    access: res.accessToken,
+    accessExpires: res.accessTokenExpiresAt,
+    refresh: res.refreshToken,
+  };
+}
+
+export async function createClient(options) {
+  const res = await new Connection()
+    .post()
+    .oauth()
+    .client()
+    .data({
+      redirectURL: options.redirectURL,
+      email: options.email,
+      name: options.name,
+    })
+    .call();
+
+  OAuthManager.setClientID(res.clientId);
+  OAuthManager.setClientRedirect(options.redirectURL);
+
+  return {
+    clientID: res.clientId,
+    ClientRedirect: options.redirectURL,
+  };
+}
