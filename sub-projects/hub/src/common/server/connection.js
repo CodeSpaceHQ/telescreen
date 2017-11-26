c4409c781ec3be01b9a6649a705a0fc664758972
NilsG-S <nils.griswold.steiner@gmail.com>
2017-11-26 15:08:01 -0600

    Simplified connection.js

--- sub-projects/hub/src/common/server/connection.js
+++ sub-projects/hub/src/common/server/connection.js
@@ -1,10 +1,4 @@
 import axios from 'axios';
-import logger from 'utils/logger.js';
-
-if (ENV === 'dev') {
-  // Ensures session cookie is stored and sent with requests during dev
-  axios.defaults.withCredentials = true;
-}
 
 /**
  * The connection information for the server hosting the API.
@@ -13,39 +7,11 @@ if (ENV === 'dev') {
  * @param {number} timeout - Time in ms that each call should take
  */
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:3000/api', // TODO(asclines): Move this to env
+  baseURL: 'http://127.0.0.1:3000/api',
   timeout: 5000,
 });
 
 instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-/**
- * Promise callback for when API call finishes
- *
- * @typedef {function} OnApiCallFinished
- * @param {Object} - Response from API
- * @param {number} - Response status code
- */
-
-/**
- * Handles errors from API and logs them
- *
- * @param {Object} err - The error object recieved from Axios
- * @param {OnApiCallFinished} onError - Called with the correct error object
- */
-const errorHandler = (err, onError) => {
-  if (err.response) {
-    // Server responded with a status code outside 2xx range.
-    logger.error(err.response, 'Response error');
-  } else if (err.request) {
-    // No response recieved
-    logger.error(err.request, 'No response recieved');
-  } else {
-    // Who knows
-    logger.error(err.message, 'Request error');
-  }
-  onError(err);
-};
 
 /**
  * This class allows for easy building of a request to the API.
@@ -62,22 +28,14 @@ const errorHandler = (err, onError) => {
  *   });
  */
 export default class Connection {
-
   constructor() {
     this.config = {
       data: {},
       params: {},
     };
 
-    this.status = 0;
-
     this.url = '/';
     this.method = 'get';
-  }
-
-  responseHandler(response, onSuccess) {
-    onSuccess(response.data);
-    this.response = response;
   }
 
   setMethod(method) {
@@ -85,8 +43,13 @@ export default class Connection {
     return this;
   }
 
-  setUrl(url) {
+  replaceURL(url) {
     this.url = url;
+    return this;
+  }
+
+  pushURL(url) {
+    this.url += url;
     return this;
   }
 
@@ -98,22 +61,15 @@ export default class Connection {
 
   get() { return this.setMethod('get'); }
 
-  users() { return this.setUrl('/users'); }
+  users() { return this.replaceURL('/users'); }
 
-  auth() { return this.setUrl('/auth'); }
+  oauth() { return this.replaceURL('/oauth'); }
 
-  events() { return this.setUrl('/events'); }
+  token() { return this.pushURL('/token'); }
 
-  reports() { return this.setUrl('/reports'); }
+  authorize() { return this.pushURL('/authorize'); }
 
-  /**
-   * Helper method to add `/all` to endpoint.
-   * @returns {*}
-   */
-  all() {
-    this.endpoint = 'all';
-    return this;
-  }
+  client() { return this.pushURL('/client'); }
 
   /**
    * Sets the body of the request object
@@ -141,18 +97,10 @@ export default class Connection {
     return this;
   }
 
-  call(onSuccess, onError) {
+  call() {
     this.config.method = this.method;
-    if (this.endpoint) {
-      this.config.url = `${this.url}/${this.endpoint}`;
-    } else {
-      this.config.url = this.url;
-    }
+    this.config.url = this.url;
 
-    instance(this.config).then((res) => {
-      this.responseHandler(res, onSuccess);
-    }).catch((err) => {
-      errorHandler(err, onError);
-    });
+    return instance(this.config);
   }
 }
