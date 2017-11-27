9e912f2e7643febe002907d8ebf60eb55dfa6953
jowoodson <joyellealina@gmail.com>
2017-11-27 15:43:20 -0600

    added basic routing to buttons on home screen, will clean later

--- sub-projects/hub/src/app/components/home/home-page.jsx
+++ sub-projects/hub/src/app/components/home/home-page.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-// import { HashRouter, Route, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { Button, Segment, Divider } from 'semantic-ui-react';
 
 import '../app.css';
 
 export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navigateToAddAdmin: false,
+      navigatetoAddPOI: false,
+    };
+  }
   render() {
+    if (this.state.navigateToAddAdmin) {
+      return <Redirect to='/add-admin' />;
+    } else if (this.state.navigatetoAddPOI) {
+      return <Redirect to='/add-poi' />;
+    }
     return (
       <div className='centered'>
         <Segment padded>
@@ -15,6 +27,7 @@ export default class HomePage extends React.Component {
             primary
             fluid
             size='large'
+            onClick={() => this.setState({ navigateToAddAdmin: true })}
           />
           <Divider hidden />
           <Button
@@ -23,6 +36,7 @@ export default class HomePage extends React.Component {
             primary
             fluid
             size='large'
+            onClick={() => this.setState({ navigatetoAddPOI: true })}
           />
         </Segment>
       </div>
