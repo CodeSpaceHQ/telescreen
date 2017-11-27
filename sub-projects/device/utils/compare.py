fde6c0976027ca42a31f1e28635b4a3d317d2f50
camcarlton <cameron.carlton@ttu.edu>
2017-11-27 14:55:31 -0600

    Compare Faces with multi-faces
    compare function now has the capabillities to run through source images to check target image agansit it. If the user is found the program will display "Face match found with <image_name>". Otherwise the program will display "Face doesn't match with <image_name>". If there are multiple faces in the target image the program will still know whether there is or is not a match with the current source image.

--- sub-projects/device/utils/compare.py
+++ sub-projects/device/utils/compare.py
@@ -8,25 +8,37 @@ class Rekgoniton(object):
 	"""
 	def __init__(self):
 		self.client = boto3.client("rekognition")
+		self.image_list = ['cameron.jpg','rachel.jpg'];
 
 	def compare(self, img):
 		"""
 		Compares a face from the S3 bucket to a passed in image represented in
-		bytes/
+		bytes.
 		:param img: target image for comparing
 		"""
-		response = self.client.compare_faces(
-			SourceImage={
-				'S3Object':{
-					'Bucket': 'knownfaces',
-					'Name': 'picture.jpg'
-				}
-			},
-			TargetImage={
-				'Bytes': img
-			},
-			SimilarityThreshold=80,
-		)
+		i = 0
+		while i < len(self.image_list):
+			response = self.client.compare_faces(
+				SourceImage={
+					'S3Object':{
+						'Bucket': 'knownfaces',
+						'Name': self.image_list[i]
+						}
+				},
+				TargetImage={
+					'Bytes': img
+				},
+				SimilarityThreshold=80,
+			)
 
-		if response['FaceMatches'] == []:
-			print('No match found')
+			if response['FaceMatches'] != []:
+				name = self.image_list[i]
+				name = name.strip('.jpg')
+				print('Face match found with '+name)
+
+			elif response['FaceMatches'] == []:
+				name = self.image_list[i]
+				name = name.strip('.jpg')
+				print('No face match with '+ name)
+
+			i += 1
--- sub-projects/device/utils/test_compare.py
+++ sub-projects/device/utils/test_compare.py
@@ -4,15 +4,18 @@ import base64
 rek = Rekgoniton()
 
 #this should return 'No match found'
-with open("caleb.jpg", "rb") as imageFile: #opens the image and stores the binary code in b
+
+with open("cameron.jpg", "rb") as imageFile: #opens the image and stores the binary code in b
   f = imageFile.read()
   b = bytearray(f)
 
 rek.compare(b) #calls the compare function
 
+"""
 #this should run successfully and return nothing as of now
-with open("cameron.jpg", "rb") as imageFile:
+with open("test_multiple.jpg", "rb") as imageFile:
   f = imageFile.read()
   b = bytearray(f)
 
-Rekgoniton.compare(b)
+rek.compare(b)
+"""
