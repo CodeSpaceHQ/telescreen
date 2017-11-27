e4929636f906dfff80f163824582b95bd2991c65
NilsG-S <nils.griswold.steiner@gmail.com>
2017-11-26 19:20:08 -0600

    Minor change

--- sub-projects/hub/src/common/server/connection.js
+++ sub-projects/hub/src/common/server/connection.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
 /**
  * The connection information for the server hosting the API.
  *
@@ -9,9 +11,6 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'http://127.0.0.1:3000/api',
   timeout: 5000,
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
 });
 
 /**
