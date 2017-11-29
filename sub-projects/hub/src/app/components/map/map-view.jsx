2eb73ac8ffaf2936cd149d5f4adb209c16778686
Rachel <rachelcramblitt@gmail.com>
2017-11-29 00:23:57 -0600

    two markers on map with infowindows added

--- sub-projects/hub/src/app/components/map/map-view.jsx
+++ sub-projects/hub/src/app/components/map/map-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import { Icon } from 'semantic-ui-react';
 
 const divStyle = {
   position: 'absolute',
@@ -9,10 +10,31 @@ const divStyle = {
 
 const MyMapComponent = withScriptjs(withGoogleMap(props =>
   (<GoogleMap
-    defaultZoom={15}
-    defaultCenter={{ lat: 33.587499, lng: -101.875706 }}
+    defaultZoom={17}
+    defaultCenter={{ lat: 33.586275, lng: -101.874726 }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 33.587499, lng: -101.875706 }} />}
+    <Marker
+      position={{ lat: 33.587479, lng: -101.875706 }}
+      onClick={props.handleMarkerClick}
+    >
+      {<InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          <Icon name='user' /> POI 1 <br />
+          <Icon name='clock' /> 11/26/2017 @ 11:05 am
+        </div>
+      </InfoWindow>}
+    </Marker>
+    <Marker
+      position={{ lat: 33.586583, lng: -101.874045 }}
+      onClick={props.handleMarkerClick}
+    >
+      {<InfoWindow onCloseClick={props.onToggleOpen}>
+        <div>
+          <Icon name='user' /> POI 2 <br />
+          <Icon name='clock' /> 11/23/2017 @ 1:23 pm
+        </div>
+      </InfoWindow>}
+    </Marker>
   </GoogleMap>
   ),
 ));
@@ -24,11 +46,26 @@ export default class MapView extends React.PureComponent {
       isMarkerShown: false,
     };
   }
+  componentDidMount() {
+    this.delayedShowMarker();
+  }
+
+  delayedShowMarker() {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true });
+    }, 0);
+  }
+
+  handleMarkerClick() {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
+  }
 
   render() {
     return (
       <div style={divStyle}>
         <MyMapComponent
+          onClick={this.handleMarkerClick}
           googleMapURL='https://maps.googleapis.com/maps/api/js?key=AIzaSyBRbcW5LbXSk8JlzbrPuSICH0dOQncmCfI&v=3.exp&libraries=geometry,drawing,places'
           loadingElement={<div style={{ height: '100%' }} />}
           containerElement={<div style={{ height: '100%' }} />}
