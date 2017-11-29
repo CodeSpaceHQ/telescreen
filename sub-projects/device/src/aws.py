d7eb3b6def2355a9ff3d758b9d927bbee0ce578b
camcarlton <cameron.carlton@ttu.edu>
2017-11-29 13:40:17 -0600

    S3 Intergation and Suggested FIxes
    Incorporated fixes suggested by the PR review. Also intergated functionallity to grab all the files from the knownfaces bucket and store it into a list.

--- sub-projects/device/src/aws.py
+++ sub-projects/device/src/aws.py
@@ -1,42 +1,58 @@
 import boto3
 import json
-from PIL import Image
 
-class Rekgoniton(object):
-	"""
-	A class that creates a client object for access to AWS Rekgoniton.
-	"""
-	def __init__(self):
-		self.client = boto3.client("rekognition")
-		self.image_list = ['cameron.jpg','rachel.jpg']
+class AWS(object):
+    """
+    A class that creates a session for AWS access and a list for S3 images
+    """
+    def __init__(self):
+        self.session = boto3.Session(profile_name='default')
+        self.image_list = []
+        self.getObjects()
 
-	def compare(self, img):
-		"""
-		Compares a face from the S3 bucket to a passed in image represented in
-		bytes.
-		:param img: target image for comparing
-		"""
-		i = 0
-		while i < len(self.image_list):
-		    response = self.client.compare_faces(
-			    SourceImage={
-				    'S3Object':{
-				    'Bucket': 'knownfaces',
-				    'Name': self.image_list[i]
-					}
-				},
-			    TargetImage={
-					'Bytes': img
-				},
-				SimilarityThreshold=80,
-			)
+    def getObjects(self):
+        """
+        Get a list of all objects from knownfaces S3 bucket and
+        append them to the image_list
+        """
+        s3 = self.session.client('s3')
+        response = s3.list_objects_v2(
+            Bucket='knownfaces'
+        )
 
-    	if response['FaceMatches'] != []:
-        	name = self.image_list[i]
-			name = name.strip('.jpg')
-			return True, name
+        for contents in response['Contents']:
+            self.image_list.append(contents['Key'])
 
-		else:
-			return False, ''
+    def compare(self, img):
+        """
+        Compares a face from the S3 bucket to a passed in image
+        represented in bytes.
+        :param img: target image for comparing
+        """
+        if (self.image_list) is None:
+            raise Execption('Image_List should not be empty.')
 
-		i += 1
+        names = []
+        rek = self.session.client('rekognition')
+
+        for source in self.image_list:
+            response = rek.compare_faces(
+                SourceImage = {
+                    'S3Object':{
+                        'Bucket': 'knownfaces',
+                        'Name': source
+                    }
+                },
+                TargetImage = {
+                    'Bytes': img
+                },
+                SimilarityThreshold=80,
+            )
+
+            if response['FaceMatches']:
+                name = source.strip('.jpg')
+                names.append(name)
+
+        if names: return True, names
+
+        else: return False, names
