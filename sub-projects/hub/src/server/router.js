40da304327301376c80d230ef42c7462836e9028
camcarlton <cameron.carlton@ttu.edu>
2017-11-29 18:53:51 -0600

    fixes

--- sub-projects/hub/src/server/router.js
+++ sub-projects/hub/src/server/router.js
@@ -3,10 +3,12 @@ const express = require('express');
 // Routers.
 const auth = require('./auth/auth-router.js');
 const user = require('./users/user-router.js');
+const poi = require('./poi/poi-router.js');
 
 // App routes.
 const router = express.Router();
 router.use('/auth', auth.router);
 router.use('/users', user.router);
+router.use('/poi', poi.router);
 
 module.exports = router;
